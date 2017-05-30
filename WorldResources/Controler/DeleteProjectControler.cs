5f5a7eebddfb8ebb43337174cb80af20feed9255
milosrs <milosribar@yahoo.com>
2017-05-30 03:29:54 +0200

    Potrebno je pronaci bug-ove

---
+++ WorldResources/Controler/DeleteProjectControler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldResources.Controler
+{
+    class DeleteProjectControler
+    {
+        public DeleteProjectControler()
+        {
+            GlowingEarth.getInstance().getMaster().getResources().Clear();
+            GlowingEarth.getInstance().getMaster().getTypes().Clear();
+            GlowingEarth.getInstance().getMaster().getTags().Clear();
+            GlowingEarth.getInstance().getMaster().getMapItems().Clear();
+            GlowingEarth.getInstance().getMaster().setTitle(GlowingEarth.getInstance().getMaster().getTitle() + "*");
+        }
+    }
+}
